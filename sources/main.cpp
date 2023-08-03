932bedd9f8927a997d47c2a0cac810c5a4ded8b1
sdi-lega <SDLMPersonal@gmail.com>
2023-08-03 05:25:21 +0200

    trying to send html code with header

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 22:31:13 by sdi-lega          #+#    #+#             */
-/*   Updated: 2023/08/03 04:44:26 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2023/08/03 05:24:42 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ int	main(int argc, char const *argv[])
 		std::cerr << "Not enough  arguments." << std::endl;
 		return (1);
 	}
-	MySocket test(9000);
+	MySocket test(9001);
 	while (1)
 	{
+		// std::string message = "<html><body>testing</body></html>";
+		std::string message = "HTTP/1.1 200 OK\nContent-Type: text/html; charset=UTF-8\n\n<html><body>testing</body></html>";
 		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
 		int new_socket, valread;
 		new_socket = test.accept_connection();
@@ -33,7 +35,7 @@ int	main(int argc, char const *argv[])
 		char buffer[30000] = {0};
         valread = read( new_socket , buffer, 30000);
         printf("%s\n",buffer );
-        write(new_socket , "hello" , 5);
+        write(new_socket , message.c_str(), strlen(message.c_str()));
         printf("------------------Hello message sent-------------------\n");
         close(new_socket);
 		// ouvrir localhost:9000 sur son navigateur crée une connection
