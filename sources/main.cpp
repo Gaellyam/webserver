a43cbfaa6ac521d2f877ce4dbcb1dbecac9d7116
sdi-lega <SDLMPersonal@gmail.com>
2023-08-03 04:44:45 +0200

    typo

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sdi-lega <sdi-lega@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 22:31:13 by sdi-lega          #+#    #+#             */
-/*   Updated: 2023/08/03 04:39:27 by sdi-lega         ###   ########.fr       */
+/*   Updated: 2023/08/03 04:44:26 by sdi-lega         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,14 @@ int	main(int argc, char const *argv[])
 		printf("\n+++++++ Waiting for new connection ++++++++\n\n");
 		int new_socket, valread;
 		new_socket = test.accept_connection();
+		std::cout << "Connection accepted\n";
 		char buffer[30000] = {0};
         valread = read( new_socket , buffer, 30000);
         printf("%s\n",buffer );
         write(new_socket , "hello" , 5);
         printf("------------------Hello message sent-------------------\n");
         close(new_socket);
-		// ouvrir localhost:9000 sur son navigateur cree une connection
+		// ouvrir localhost:9000 sur son navigateur crée une connection
 	}
 	return (0);
 }
